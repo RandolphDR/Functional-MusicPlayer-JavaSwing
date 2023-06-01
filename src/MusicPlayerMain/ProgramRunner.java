d458b1965a618f10c256feed213a52c04db72011
ArtuDR <134755593+ArtuDR@users.noreply.github.com>
2023-06-02 07:17:24 +0800

    Update ProgramRunner.java
    Additional Comments by the Creator :)

--- src/MusicPlayerMain/ProgramRunner.java
+++ src/MusicPlayerMain/ProgramRunner.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-//Music Player GUI Project was begin May 18,2023 at 12:09 am By Randolph Soriano Saludo.
+// Music Player GUI Project was begin May 18,2023 at 12:09 am By @ArtuDR
+// This Program is currently in developing, you may encounter some bugs on it.
+// Sorry for the codes i am newbie in the Programming World :).
 
 public class ProgramRunner {
 	public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-
+               // Run This Class to Test my Java Music Project GUI
 		new MusicPlayer();;
 		
 		
